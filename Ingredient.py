4bfeee310ff0617ebb629295042d112e462c85b7
Amber Doezejager <amber.doezejager@ru.nl>
2025-03-12 18:34:01 +0100

    Add comments Ingredient & Shelf

--- Ingredient.py
+++ Ingredient.py
@@ -2,7 +2,18 @@ from datetime import date
 
 
 class Ingredient:
+    """
+    An ingredient.
+    Includes a name, a max temperature at which the product should be kept, an experation date and a quantity.
+    """
     def __init__(self, name: str, max_temperature: int):
+        """
+        Initializes an ingredient object.
+
+        Args:
+            name (str) - Name of the ingredient
+            max_temperature (int) - Max temperature at which the ingredient should be kept
+        """
         self.name = name
         self.max_temperature = max_temperature
         self.__expiration_date = date.today()
@@ -10,19 +21,27 @@ class Ingredient:
 
     @property
     def quantity(self):
+        """Returns the quantity of the ingredient"""
         return self.__quantity
 
     @quantity.setter
     def quantity(self, quantity):
+        """Sets the quantity of an ingredient. 
+        Raises a ValueError if quantity is not higher than zero. """
         if quantity > 0:
             self.__quantity = quantity
+        else:
+            raise ValueError("Quantity should be higher than zero.")
 
     @property
     def expiration_date(self):
+        """Returns the expiration date of the ingredient."""
         return self.__expiration_date
 
     @expiration_date.setter
     def expiration_date(self, day: date):
+        """Sets the expiration date of an ingredient.
+        If the day is not convertable to a date it raises a ValueError."""
         try:
             date(day)
             self.__expiration_date = day
@@ -30,34 +49,41 @@ class Ingredient:
             raise ValueError("This is not of the form (year, month, day)")
 
     def add(self, amount: int):
+        """Adds an amount to the quantity of an ingredient.
+        Raises a ValueError if amount is not higher than zero."""
         if amount > 0:
             self.__quantity += amount
         else:
             raise ValueError("Amount not larger than zero")
 
     def remove(self, amount: int):
+        """Remove an amount of the quantity of an ingredient.
+        Raises a ValueError if amount is not higher than zero or is lower than the current quantity"""
         if amount <= self.__quantity and amount > 0:
             self.__quantity -= amount
         else:
-            raise ValueError(
-                f"Amount not higher than zero or not lower than or equal to current quantity, which is {self.__quantity}")
+            raise ValueError(f"Amount not higher than zero or not lower than or equal to current quantity, which is {self.__quantity}")
 
     def __str__(self):
         return f"{self.name}: {self.quantity} g. Kept at {self.temperature} degrees till {self.expiration_date}"
     
     def __eq__(self, another):
+        """Checks if the ingredient is the same.
+        For this everything should be equal except the quantity."""
         if self.name == another.name and self.expiration_date == another.expiration_date and self.temperature == another.temperature:
             return True
         else:
             return False
 
     def __le__(self, another):
+        """Checks if there is less or equal quantity of an ingredient."""
         if self.__quantity <= another.__quantity:
             return True
         else:
             return False
 
     def __ge__(self, another):
+        """Checks if there is more or equal quantity of an ingredient."""
         if self.__quantity >= another.__quantity:
             return True
         else:
--- Shelf.py
+++ Shelf.py
@@ -1,23 +1,29 @@
 from Ingredient import Ingredient
 
 class Shelf():
+    """A shelf in which ingredients can be stored.
+    Has a storage space."""
     def __init__(self, storage_space):
+        """Sets up a shelf object. Arg: storage_space (int) - Amount of storage the shelf has."""
         self.__storage_space = storage_space
         self._ingredients = []
 
     def in_shelf(self, check_ingredient: Ingredient):
+        """Checks if a certain ingredient is already in the shelf."""
         for ingredient in self._ingredients:
             if ingredient == check_ingredient:
                 return True
         return False
 
     def storable(self, ingredient: Ingredient):
+        """Checks if there is enough space on the shelf for an ingredient."""
         if ingredient.__quantity <= self.__storage_space:
             return True
         else:
             return False
 
     def add_ingredient(self, new_ingredient: Ingredient):
+        """Adds an ingredient to the shelf. Raises a ValueError if there is not enough space."""
         if self.storable:
             for ingredient in self._ingredients:
                 if ingredient == new_ingredient:
@@ -28,7 +34,9 @@ class Shelf():
             raise ValueError("Not enough space in shelf.")
 
     def remove_ingredient(self, ingredient: Ingredient):
-        # same thing as above if i in list: is way easier
+        """Removes an ingredient from the shelf. 
+        Raises a ValueError if the ingredient isn't on the shelf.
+        Also raise a ValueError if the specified amount of ingredient isn't in the shelf."""
         for item in self._ingredients:
             if item == ingredient:
                 if ingredient <= item:
