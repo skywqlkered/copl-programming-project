55561bb9fba9027631afabee1dd63ecacdd8d504
Julian <113358009+skywqlkered@users.noreply.github.com>
2025-03-12 14:45:24 +0100

    Addes comments to shelf and removed removed def statement in Ingredient

--- Ingredient.py
+++ Ingredient.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Ingredient:
     def __init__(self, name: str, stored_temperature: int):
         self.name = name
@@ -7,10 +8,10 @@ class Ingredient:
         self.__expiration_date = date.today()
         self.__quantity = 0
 
-    @property    
+    @property
     def quantity(self):
         return self.__quantity
-    
+
     @quantity.setter
     def quantity(self, quantity):
         if quantity > 0:
@@ -19,7 +20,7 @@ class Ingredient:
     @property
     def expiration_date(self):
         return self.__expiration_date
-    
+
     @expiration_date.setter
     def expiration_date(self, day: date):
         try:
@@ -33,20 +34,19 @@ class Ingredient:
             self.__quantity
         else:
             raise ValueError("Amount not larger than zero")
-        
+
     def remove(self, amount: int):
         if amount <= self.__quantity and amount > 0:
             self.__quantity -= amount
         else:
-            raise ValueError(f"Amount not higher than zero or not lower than or equal to current quantity, which is {self.__quantity}")
+            raise ValueError(
+                f"Amount not higher than zero or not lower than or equal to current quantity, which is {self.__quantity}")
 
     def __str__(self):
         return f"{self.name}: {self.quantity} g. Kept at {self.temperature} degrees till {self.expiration_date[0]}-{self.expiration_date[1]-self.expiration_date[2]}"
-    
+
     def __eq__(self, another):
         if self.name == another.name and self.expiration_date == another.expiration_date and self.temperature == another.temperature:
             return True
         else:
             return False
-    
-    def 
--- Shelf.py
+++ Shelf.py
@@ -6,12 +6,14 @@ class Shelf():
         self._ingredients = []
 
     def in_shelf(self, check_ingredient: Ingredient):
+        # rewrite this to if ingredient in self._ingredients: return true because reasons
         for ingredient in self._ingredients:
             if ingredient == check_ingredient:
                 return True
         return False
 
     def add_ingredient(self, new_ingredient: Ingredient):
+        # this is all wrong and doesnt make sense
         for ingredient in self._ingredients:
             if ingredient == new_ingredient:
                 ingredient.add(new_ingredient.quantity)
@@ -19,6 +21,7 @@ class Shelf():
         self._ingredients.append(new_ingredient)
 
     def remove_ingredient(self, ingredient: Ingredient):
+        # same thing as above if i in list: is way easier
         for item in self._ingredients:
             if item == ingredient:
                 raise NotImplementedError
